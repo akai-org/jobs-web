8115cdd3dad1ddfc22d027f1864a7de79d3912e6
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-10-05 18:59:59 +0200

    remove unused addons and stories

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,2 @@
 import '@storybook/addon-actions/register';
-// import 'storybook-addon-jsx/register';
 import 'storybook-addon-styled-component-theme/dist/register';
--- src/components/LinkCTA/index.stories.js
+++ src/components/LinkCTA/index.stories.js
@@ -10,54 +10,20 @@ const clickPrimaryLinkAction = action('clickPrimaryLinkAction');
 
 storiesOf('LinkCTA', module)
   .addWithJSX(
-    'standard LinkCTA 1', 
-    StoryInfo(`
-      this story has access to context 
-
-      ~~~js
-        <LinkCTA>
-          Standard Link 1
-        </LinkCTA>
-      ~~~
-      `, 
-      false,  // true - show inline, false - show button
-      true,   // show/hide header
-      true    // show/hide component source
-    )(() => <LinkCTA onClick={clickStandardLinkAction}>Standard Link 1</LinkCTA>)
-  )
-  .addWithJSX(
-    'standard LinkCTA 2', 
-    StoryInfo(`
-      this story has access to context
-
-      ~~~js
-        <LinkCTA>
-          Standard Link 2
-        </LinkCTA>
-      ~~~
-      `, 
+    'standard LinkCTA', 
+    StoryInfo(
+      null,   // component description
       true,   // true - show inline, false - show button
       true,   // show/hide header
       true    // show/hide component source
-    )(() => <LinkCTA onClick={clickStandardLinkAction}>Standard Link 2</LinkCTA>)
+    )(() => <LinkCTA onClick={clickStandardLinkAction}>Standard Link</LinkCTA>)
   )
-  .addWithJSX('standard LinkCTA 3', () => getStoryStandardButton())
-  .addWithJSX('primary LinkCTA', () => getStoryPrimaryButton());
-
-const getStoryStandardButton = StoryInfo(`
-  this story has not access to context, why?
-
-  when context is undefined we don't see header
-
-  ~~~js
-    <LinkCTA>
-      Standard Link 3
-    </LinkCTA>
-  ~~~
-  `, 
-  true,   // true - show inline, false - show button
-  true,   // show/hide header
-  true    // show/hide component source
-)(() => <LinkCTA onClick={clickStandardLinkAction}>Standard Link 3</LinkCTA>);
-
-const getStoryPrimaryButton = () => <LinkCTA primary onClick={clickPrimaryLinkAction}>Primary Link</LinkCTA>;
+  .addWithJSX(
+    'primary LinkCTA', 
+    StoryInfo(
+      null,   // component description
+      true,   // true - show inline, false - show button
+      true,   // show/hide header
+      true    // show/hide component source
+    )(() => <LinkCTA primary onClick={clickPrimaryLinkAction}>Primary Link</LinkCTA>)
+  );

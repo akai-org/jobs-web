214466aff0258c81a4068e30bf8c9c5665212ab7
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2018-10-05 18:03:23 +0200

    remove unnecessary code

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,19 +3,5 @@ import { createAction, createSimpleAction } from './actionCreator';
 export const SET_MENU_OPENED = "SET_MENU_OPENED";
 export const TOGGLE_MENU_OPENED = "TOGGLE_MENU_OPENED";
 
-// export const setMenuOpened = state => ({
-//   type: SET_MENU_OPENED,
-//   payload: state
-// });
-
-// export const toggleMenuOpened = () => ({
-//   type: TOGGLE_MENU_OPENED,
-//   payload: {}
-// });
-
 export const setMenuOpened = state => createAction(SET_MENU_OPENED, state);
 export const toggleMenuOpened = () => createSimpleAction(TOGGLE_MENU_OPENED);
-
-// or another way
-// export const toggleMenuOpened = () => createAction(TOGGLE_MENU_OPENED);
-// works exactly the same as example above

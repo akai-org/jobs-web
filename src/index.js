64ec788fd5ff2f6e26cc19d77a60015520f73333
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2019-01-20 15:07:30 +0100

    add firebase

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { render } from "react-dom";
 
+import "./firebase/firebase";
+
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 

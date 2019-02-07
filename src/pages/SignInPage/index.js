cf8327c8bbbe44e6061d7bc5710362eb9858df7d
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2019-02-07 21:31:04 +0100

    styling SignInForm

--- src/pages/SignInPage/index.js
+++ src/pages/SignInPage/index.js
@@ -3,16 +3,26 @@ import PropTypes from "prop-types";
 import { Link, withRouter } from "react-router-dom";
 import { Form, Field } from "react-final-form";
 import { compose } from "recompose";
+import styled from "styled-components";
 
 import { withFirebase } from "../../firebase";
 import Container from "../../components/Container";
+import { Field as CustomField } from "../../components/Form";
+import Heading from "../../styled-components/Heading";
+import Button from "../../styled-components/Button";
+
+const StyledWrapper = styled.section`
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  margin-top: 50px;
+`;
+
+const FormWrapper = styled.form`
+  width: 320px;
+`;
 
 const SignInPage = ({ firebase, history }) => {
-  // async function showResults({ email, password }) {
-  //   firebase.login(email, password);
-  //   return { email, password };
-  // }
-
   const onSubmitHandler = values =>
     firebase
       .login(values.email, values.password)
@@ -23,33 +33,34 @@ const SignInPage = ({ firebase, history }) => {
 
   return (
     <Container>
-      <Form onSubmit={onSubmitHandler}>
-        {({ handleSubmit, submitting }) => (
-          <form onSubmit={handleSubmit}>
-            <Field name="email" placeholder="email address" validate={required}>
-              {({ input, meta, placeholder }) => (
-                <div>
-                  <label>Email</label>
-                  <input {...input} placeholder={placeholder} />
-                  {meta.error && meta.touched && <span>{meta.error}</span>}
-                </div>
-              )}
-            </Field>
-            <label>Password</label>
-            <Field
-              name="password"
-              component="input"
-              type="password"
-              placeholder="password"
-              validate={required}
-            />
-            <button type="submit" disabled={submitting}>
-              Submit
-            </button>
-          </form>
-        )}
-      </Form>
-      <Link to="/">Wróć do strony głównej</Link>
+      <StyledWrapper>
+        <Heading>Logowanie</Heading>
+        <Form onSubmit={onSubmitHandler}>
+          {({ handleSubmit, submitting }) => (
+            <FormWrapper onSubmit={handleSubmit}>
+              <Field
+                name="email"
+                component={CustomField}
+                type="email"
+                placeholder="Email"
+                validate={required}
+              />
+
+              <Field
+                name="password"
+                component={CustomField}
+                type="password"
+                placeholder="Hasło"
+                validate={required}
+              />
+              <Button type="submit" disabled={submitting}>
+                Zaloguj
+              </Button>
+            </FormWrapper>
+          )}
+        </Form>
+        <Link to="/">Wróć do strony głównej</Link>
+      </StyledWrapper>
     </Container>
   );
 };
